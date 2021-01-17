f022e00b734d92861c2233d5c94b14089750c578
Chris Field <chris@ctafield.com>
2021-01-17 08:43:02 +0000

    white background black text. different icons

--- routes/weatherImage.js
+++ routes/weatherImage.js
@@ -28,32 +28,46 @@ router.get('/user', async function (req, res, next) {
     const canvas = createCanvas(width, height)
     const context = canvas.getContext('2d')
 
-    context.fillStyle = '#000'
+    context.fillStyle = '#fff'
     context.fillRect(0, 0, 400, 300)
 
     context.textAlign = 'center'
-    context.fillStyle = '#fff'
 
     // currently temp
     context.font = 'normal 24px White Rabbit, serif'
+    context.fillStyle = '#000'
     context.fillText(`Currently`, 80, 35, 150)
 
     context.font = 'normal 60px White Rabbit, serif'
+    context.fillStyle = '#000'
     context.fillText(`${weather.current.temp.toFixed(0)}'c`, 80, 100, 125)
 
     // currently feels like
     context.font = 'normal 24px White Rabbit, serif'
+    context.fillStyle = '#000'
     context.fillText(`Feels Like`, 305, 35, 150)
 
     context.font = 'normal 60px White Rabbit, serif'
+    context.fillStyle = '#000'
     context.fillText(`${weather.current.feels_like.toFixed(0)}'c`, 305, 100, 125)    
 
-    const imageUrl =`http://openweathermap.org/img/wn/${weather.current.weather[0].icon}@2x.png`;
+    const imageUrl =`assets/icons/${weather.current.weather[0].icon}.png`;
     const image = await loadImage(imageUrl)
-    context.drawImage(image, 150, 100, 100, 100)
+    context.drawImage(image, 150, 90, 100, 100)
 
     context.font = 'normal 60px White Rabbit, serif'
-    context.fillText(`${weather.current.weather[0].main}`, 200, 250, 125)    
+    context.fillStyle = '#000'
+    context.fillText(`${weather.current.weather[0].main}`, 200, 240, 125)    
+
+
+    context.font = 'normal 12px White Rabbit, serif'
+    context.fillStyle = '#000'
+
+    const cd = new Date()
+    const formatted_date =  cd.getHours() + ":" + cd.getMinutes() + " " + cd.getDate() + "/" + (cd.getMonth() + 1) + "/" + cd.getFullYear()
+
+    context.fillText(`Updated ${formatted_date}`, 200, 275, 125)    
+
 
     res.setHeader('Content-Type', 'image/png');
     res.send(canvas.toBuffer('image/png'));
